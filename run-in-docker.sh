c3423fc4e8f9d15f72acfaea4a811b037fa1b4b3
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2017-03-09 16:48:48 -0500

    Add support for mounting volume with spec in it with docker

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -exo pipefail
 
+echo $spec_folder
 cd "$(dirname ${BASH_SOURCE})"
 
 maven_cache_repo="${HOME}/.m2/repository"
@@ -11,6 +12,7 @@ docker run --rm -it \
         -w /gen \
         -e GEN_DIR=/gen \
         -v "${PWD}:/gen" \
+        -v "${SPEC_FOLDER:-$PWD}:/spec" \
         -v "${maven_cache_repo}:/root/.m2/repository" \
         --entrypoint /gen/docker-entrypoint.sh \
         maven:3-jdk-7 "$@"
